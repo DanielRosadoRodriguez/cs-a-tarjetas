e487c395eeaba8eddc603598b4eaa54cb87ef202
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2024-05-23 03:40:34 -0600

    docs: add comments for classes

--- cardhub/domain/authenticator.py
+++ cardhub/domain/authenticator.py
@@ -4,6 +4,9 @@ from cardhub.exceptions.IncorrectPasswordException import IncorrectPasswordExcep
 from cardhub.models import User
 
 class Authenticator:
+    '''
+    Class that authenticates a user.
+    '''
     def __init__(self, 
                  users:list[User],
                  email:str,
@@ -14,8 +17,14 @@ class Authenticator:
         self._password:str = password
 
     def authenticate_user(self):
+        '''
+        IN: self, users, email, password
+        OUT: User
+        It authenticates a user by checking if the email is registered'''
         try:
+            # We try to get an user with the given email, if it is not found, we raise an exception
             found_user = User.objects.get(_email=self._email)
+            # Look if the password is the same as the one the given email has
             if found_user.get_password() == self._password:
                 return found_user
             else:
@@ -25,12 +34,22 @@ class Authenticator:
             
 
     def _is_email_registered(self):
+        '''
+        IN: self
+        OUT: bool
+        It checks if the email is registered in the system.
+        '''
         for user in self._users:
             if user.get_email() == self._email:
                 return True
         raise EmailNotRegisteredException(self._email)
 
     def _is_password_correct(self):
+        '''
+        IN: self
+        OUT: bool
+        It checks if the password is correct.
+        '''
         for user in self._users:
             if user.get_email() == self._email:
                 if user.get_password() == self._password:
